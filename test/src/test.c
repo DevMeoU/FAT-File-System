679f52cdc058a6b8375920d805a0279537cf2c8c
DEESOL <ducson9112k@gmail.com>
2025-03-04 20:33:49 +0700

    Update code test

--- test/src/test.c
+++ test/src/test.c
@@ -1,27 +1,67 @@
-#include <stdio.h>
+#include "test.h"
 #include "ip_driver.h"
 
 int main(void) {
     printf("Hello from test!\n");
+    
+    #ifdef TEST_DRIVER
+        int result = test_ip_driver();
+        
+        assert(result == -1);
+    #endif
+
+    return 0;
+}
+
+int test_ip_driver(void)
+{
+    int result = -1;
     /* Path to floppy.img */
     const char * img_path = "E:\\Workspace\\project\\clone\\FATFileSystem\\images\\floppy.img";
     
     /* Initialize IP Driver */
     ip_driver_init(img_path);
     
-    /* Read from IP Driver */
+    /* Read from IP Driver from sector 0 */
     unsigned char buffer[512];
-    ip_driver_read(0x1000, buffer, 512);
-    printf("Read from IP Driver: %s\n", buffer);
+    result = ip_driver_read(0x0, buffer, 512);
+    assert(result == 0);
+    printf("Read from IP Driver from sector 0: %s\n", buffer);
     
-    /* Print to IP Driver */
-
-    for(int i = 0, j = 0; i < 512; i++, j++) {
-        printf("%02X ", buffer[i]);
-        if (j != 0 && j % 16 == 0) {
-            printf("\n");
+    #ifdef HEX_PRINT
+        /* Print to IP Driver */
+        for(int i = 0, j = 1; i < 512; i++, j++) {
+            printf("%02X ", buffer[i]);
+            if ((j > 0) && (j % 16 == 0)) {
+                printf("\n");
+            }
         }
-    }
+        printf("\n");
+    #endif
+
+    /* Read from IP Driver from sector 7 */
+    result = ip_driver_read(0x700, buffer, 512);
+    assert(result == 0);
+    printf("Read from IP Driver from sector 7: %s\n", buffer);
     
-    return 0;
+    #ifdef HEX_PRINT
+        /* Print to IP Driver */
+        for(int i = 0, j = 1; i < 512; i++, j++) {
+            printf("%02X ", buffer[i]);
+            if ((j > 0) && (j % 16 == 0)) {
+                printf("\n");
+            }
+        }
+        printf("\n");
+    #endif
+    
+    /* Close IP Driver */
+    ip_driver_close();
+
+    /* TODO: Read from IP Driver */
+    result = ip_driver_read(0x0, buffer, 512);
+    /* Expected result: -1 */
+    assert(result == -1);
+
+    return result;
 }
---
+++ test/src/test.h
@@ -0,0 +1,10 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <stdio.h>
+#include <assert.h>
+
+int test_ip_driver(void);
+
+#endif // TEST_H
+
