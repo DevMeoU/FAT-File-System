cce82fdca8b3dc78755ddcb8b9842fc2b7e8d295
DEESOL <ducson9112k@gmail.com>
2025-03-08 15:32:16 +0700

    delete unused file

--- project/build/obj/application/application.d
+++ project/build/obj/application/application.d
@@ -1,6 +1,10 @@
 build/obj/application/application.o: src/application/application.c \
- src/middleware/middleware.h src/application/application.h \
- src/utilities/log/print_color.h
-src/middleware/middleware.h:
+ src/application/application.h src/utilities/status/common_type.h \
+ src/middleware/middleware.h src/fat_driver/fat_driver.h \
+ src/ip_driver/ip_driver.h src/utilities/log/print_color.h
 src/application/application.h:
+src/utilities/status/common_type.h:
+src/middleware/middleware.h:
+src/fat_driver/fat_driver.h:
+src/ip_driver/ip_driver.h:
 src/utilities/log/print_color.h:
